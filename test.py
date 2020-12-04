8f09bdddc1dc174346d7fa2faad0d1d145e14592
LamLauChiu <42856557+LamLauChiu@users.noreply.github.com>
2020-12-04 17:38:01 +0800

    Update test.py

--- test.py
+++ test.py
@@ -11,9 +11,36 @@ pip install -r config/requirements.txt
 """
 #import yfinance as yf
 from yahooquery import Ticker
-aapl = Ticker('aapl')
 
-print(aapl.summary_detail)
-print(aapl.all_financial_data())
-aapl.all_financial_data().to_csv('Result.csv')
+symbol = '9988.HK'
+stock = Ticker(symbol)
 
+print("P/E Ratio Worked")
+print(stock.key_stats[symbol])
+
+print(stock.summary_detail)
+print(stock.earning_history)
+print(stock.earnings)
+print(stock.index_trend)
+
+types = ['PeRatio']
+#print(stock.get_financial_data(types, trailing=False)['PeRatio'])
+
+trailingPE = stock.summary_detail[symbol]['trailingPE']
+print("trailingPE:",stock.summary_detail[symbol]['trailingPE'])
+print("forwardPE:",stock.summary_detail[symbol]['forwardPE'])
+
+#PERatio = stock.index_trend[symbol]['peRatio']
+#print(PERatio)
+
+trailingEps = stock.key_stats[symbol]['trailingEps']
+print(trailingEps)
+forwardEps = stock.key_stats[symbol]['forwardEps']
+print(forwardEps)
+# print(stock.all_financial_data())
+# stock.all_financial_data().to_csv('Result.csv')
+
+PERatioWorked = trailingPE * trailingEps
+ForwardPERatioWorked = trailingPE * forwardEps
+print(PERatioWorked)
+print(ForwardPERatioWorked)
